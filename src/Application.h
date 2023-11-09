076c57cd7e4e390ae5dabd1a90854f55a1308dcc
Theresa-0328 <Theresa-Apocalypse@outlook.com>
2023-11-09 19:23:33 +0800

    添加项目文件。

---
+++ src/Application.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+
+#include <iostream>
+#include <memory>
+
+#include <Windows.h>
+#include <stdlib.h>
+#include <crtdbg.h>
+
+class Application
+{
+public:
+	Application();
+	~Application();
+
+private:
+
+};
---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+﻿#include "Application.h"
+
+#define _CRTDBG_MAP_ALLOC
+
+int main(int argc,char* argv[])
+{
+	_CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+	_CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_DEBUG);
+
+	Application app{};
+	std::cout << "Hello CMake." << std::endl;
+	return 0;
+}
