06cefd222651097bf4137ee7cd46d7739857237f
Theresa-0328 <Theresa-Apocalypse@outlook.com>
2023-12-21 22:04:06 +0800

    add image data

--- src/FirePillar.cpp
+++ src/FirePillar.cpp
@@ -18,7 +18,19 @@ void FirePillar::Start()
 
 void FirePillar::Render()
 {
-	SDL_RenderCopyEx(m_render, FirePillar_img.get(), 0, 0, 0, nullptr, flip);
+	int index{ 0 };
+
+	SDL_Rect rect3{ 74 * 4,0,74,160 };
+	SDL_Rect rect13{ 0,0,148,320 };
+	SDL_RenderCopyEx(m_render, FirePillar_img.get(), &rect3, &rect13, 0, nullptr, flip);
+
+	SDL_Rect rect1{ 128 * 5,0,128,128 };
+	SDL_Rect rect11{ 200,0,128,128 };
+	SDL_RenderCopyEx(m_render, FirePillar_explosion_img.get(), &rect1, &rect11, 0, nullptr, flip);
+
+	SDL_Rect rect2{ 19 * 2,0,19,16 };
+	SDL_Rect rect12{ 400,0,128,128 };
+	SDL_RenderCopyEx(m_render, FirePillar_move_img.get(), &rect2, &rect12, 0, nullptr, flip);
 }
 
 void FirePillar::Update()
--- src/FirePillar.h
+++ src/FirePillar.h
@@ -20,7 +20,7 @@ private:
 	SDL_RendererFlip flip = SDL_FLIP_NONE;
 
 	std::shared_ptr<SDL_Texture> FirePillar_img;
-	const std::array<int, 2> FirePillar_vec{ 0,1 };
+	const std::array<int, 2> FirePillar_vec{ 4,5 };
 
 	std::shared_ptr<SDL_Texture> FirePillar_explosion_img;
 	const std::array<int, 12> FirePillar_explosion_vec{ 0,1,2,3,4,5,7,8,9,10,11 };
